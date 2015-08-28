b922077646de9b23ec728360029872c690fe4d59
Tanner May <tannerjmay@gmail.com>
2015-08-28 10:23:35 -0600

    Cool methods

--- methods2.rb
+++ methods2.rb
@@ -1,11 +1,25 @@
 module Methods2
 	
 	# TODO - write elevenish?
-	
+	def elevenish?(n)
+		n % 11 == 0 || n % 11 == 1
+	end
+
 	# TODO - write ice_cream_party?
-	
+	def icecream_party?(i,c)
+		if(i * 2 == c || c * 2 == i)
+			return 2
+		elsif(i >= 5 && c >= 5)
+			return 1
+		elsif(i < 5 || c < 5)
+			return 0
+		end
+	end
+
 	# TODO - write successful_squirrel_party?
-	
+	def squirrel_party?(n,w);
+		if(w = 0 && n >=40 && n <= 60)
+			return 1
 	# TODO - write ticket
 
 	# TODO - write in_order?
