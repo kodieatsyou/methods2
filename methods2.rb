2f231e7c88a33f6d4573c177fee3c77b3eef6687
Tanner May <tannerjmay@gmail.com>
2015-09-01 10:21:24 -0600

    All methods pass their tests

--- methods2.rb
+++ methods2.rb
@@ -37,7 +37,7 @@ module Methods2
 		end
 		return 0
 	end
-
+	# TODO - write in_order?
 	def in_order?(a, b, c, bOk)
 		if b > a && c > b && !bOk
 			return true
@@ -46,17 +46,40 @@ module Methods2
 		end
 		return false
 	end
-				
-	# TODO - write in_order?
-
 	# TODO - write less_by_ten?
-	
+	def less_by_ten?(a, b, c)
+		if a <= b - 10 || a <= c - 10 || b <= a - 10 || b <= c - 10 || c <= a - 10 || c <= b -10
+			return true
+		end
+		return false
+	end
 	# TODO - write fizz_string
-
+	def fizz_string?(str)
+		if str[0] == "f" && str[str.length-1] == "b"
+			return "FizzBuzz"
+		elsif str[0] == "f"
+			return "Fizz"
+		elsif str[str.length-1] == "b"
+			return "Buzz"
+		end
+		return str
+	end
 	# TODO - write first_last_six?
-
+	def first_last_six?(a)
+		if a[0] == 6 || a[a.length-1] == 6
+			return true
+		end
+		return false
+	end
 	# TODO - write rotate_left
-
+	def rotate_left?(a)
+		return [a[1], a.last, a.first]
+	end
 	# TODO - write double23?
-
+	def double23?(a)
+		if a[0] == 2 && a[1] == 2 || a[0] == 3 && a[1] == 3
+			return true
+		end
+		return false
+	end
 end
